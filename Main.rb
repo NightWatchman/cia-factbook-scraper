485bd722629be37536d330835521965fd55299d1
NightWatchman <rushinge@gmail.com>
2014-02-21 16:29:47 -0800

    Write output files

--- Main.rb
+++ Main.rb
@@ -11,16 +11,25 @@ unless Dir.exists? cia_html_directory
   exit 1
 end
 
+output_dir = ARGV[1]
+unless Dir.exists? output_dir
+  Dir.mkdir output_dir, 0777
+end
+
 Dir.open cia_html_directory do |d|
   d.each do |filename|
     if filename.end_with? '.html'
       puts "Reading #{filename}"
-      STDIN.gets
 
-      File.open File.join(d.path, filename) do |f|
-        reader = MapsDotCom::CIAFactbookScraper::FactbookReader.new f
+      File.open(File.join(d.path, filename), 'r') do |input|
+        reader = MapsDotCom::CIAFactbookScraper::FactbookReader.new input
         sections = reader.sections
-        puts M101FactbookSectionRenderer.to_html sections
+
+        puts "Writing #{filename}"
+
+        File.open(File.join(output_dir, filename), 'w') do |output|
+          output.puts(M101FactbookSectionRenderer.to_html sections)
+        end
       end
     end
   end
